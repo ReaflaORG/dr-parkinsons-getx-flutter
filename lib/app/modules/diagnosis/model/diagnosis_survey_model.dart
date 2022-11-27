6ebcae2febf2e93f16b4093a3a61b5a518a5a930
jinhlee19 <jinhlee19@gmail.com>
2022-11-27 22:44:27 +0900

    Feat: diagnosis_survey_model

--- lib/app/modules/diagnosis/model/diagnosis_survey_model.dart
+++ lib/app/modules/diagnosis/model/diagnosis_survey_model.dart
@@ -1,33 +1,52 @@
+class UserSurvey {
+  // 어떤 유저가, 어떤 서베이를, 언제, 몇점을 받았는지
+  UserSurvey(
+      {required this.userId,
+      required this.surveyId,
+      this.totalScore = 0,
+      required this.createdAt});
+  int userId;
+  int surveyId;
+  int totalScore;
+  DateTime? createdAt;
+}
+
 class Survey {
   Survey({
     required this.surveyId,
     required this.quizes,
     required this.nameOfSurvey,
-    this.totalScore = 0,
   });
   //Survey({require this.})//
   // 인덱스, 문제, 정답, 사용자가 선택한 답안, // 설문시간, 점수
   int surveyId;
   String nameOfSurvey;
   List<SurveyQuiz> quizes; //20문제
-  int totalScore;
-  DateTime? created_at;
+
 }
 
 class SurveyQuiz {
   SurveyQuiz(
-      {required this.questionText,
+      {required this.surveyId,
+      required this.surveyQuizId,
+      required this.questionText,
       required this.userAnswer,
       required this.answers});
   // 문제, 사용자 값 int, 문항
+  int surveyId;
+  int surveyQuizId;
   String questionText; // 질문
-  bool userAnswer; // 유저의 답변 인덱스
+  int userAnswer; // 유저의 답변 인덱스
   List<Answer> answers;
 }
 
 class Answer {
   // 질문, int 배점
-  Answer({required this.answerScore, required this.answerText});
+  Answer(
+      {required this.surveyQuizId,
+      required this.answerScore,
+      required this.answerText});
+  int surveyQuizId;
   String answerText;
   int answerScore;
 }
