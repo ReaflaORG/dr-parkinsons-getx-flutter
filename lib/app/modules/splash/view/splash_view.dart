91e9ff132f33118001bec7c5e853f2a49cc01a0f
phongdaegi <phongdaegi@naver.com>
2022-12-18 21:07:46 +0900

    Feat: AOS Gradle Feated

--- lib/app/modules/splash/view/splash_view.dart
+++ lib/app/modules/splash/view/splash_view.dart
@@ -8,17 +8,19 @@ class SplashView extends GetView<SplashController> {
   const SplashView({super.key});
 
   @override
-  Widget build(BuildContext context) => Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.white,
-        child: Center(
-          child: Image.asset(
-            'assets/images/logo/logo.png',
-            width: 170.w,
-            // height: 100.h,
-            fit: BoxFit.contain,
-          ),
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.white,
+      child: Center(
+        child: Image.asset(
+          'assets/images/logo/logo.png',
+          width: 170.w,
+          // height: 100.h,
+          fit: BoxFit.contain,
         ),
-      );
+      ),
+    );
+  }
 }
